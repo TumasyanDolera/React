650eac5c4d5aee6bc8ebec556ec7677d2dc8e8cc
TumasyanDolera <Doleratumasyan@gmail.com>
2023-07-01 22:57:14 +0400

    react new

--- firstreact/src/components/Title.jsx
+++ firstreact/src/components/Title.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { startTransition } from "react";
 import { Component } from "react";
+
 
 class Tasks extends Component {
     state = {
         tasks: [
 
         ],
+
         ShowAddTaskForm: false,
+        showDiv: false,
         taskTitle: '',
         taskDescription: '',
         taskstartDate: '',
@@ -14,6 +17,7 @@ class Tasks extends Component {
         taskImportance: '',
         taskDeveloper: '',
     }
+
 
     handleResetForm = () => {
         this.setState({
@@ -26,20 +30,25 @@ class Tasks extends Component {
         })
     }
 
+    handleShowAddTaskForm = () => {
+        const { showAddTaskForm } = this.state;
+        this.setState({
+            showAddTaskForm: !showAddTaskForm
+        })
+    }
+
     handleSubmitForm = (event) => {
-        event.preventDefault()
-        return (
-            console.log(this.state),
-            alert("FORM SUBMITED !!!"),
-            this.setState({
-                taskTitle: '',
-                taskDescription: '',
-                taskstartDate: '',
-                taskfinishDate: '',
-                taskImportance: '',
-                taskDeveloper: '',
-            })
-        )
+        event.preventDefault();
+        // console.log(this.state),
+        const { showDiv } = this.state;
+        this.setState({
+            showDiv: !showDiv,
+        })
+        const { showAddTaskForm } = this.state;
+        this.setState({
+            ShowAddTaskForm: showAddTaskForm,
+        })
+
     }
 
     handleShowAddTaskForm = () => {
@@ -75,9 +84,12 @@ class Tasks extends Component {
 
     render() {
         const { taskTitle, taskDescription, taskstartDate, taskfinishDate,
-            taskImportance, taskDeveloper, ShowAddTaskForm, handleSubmitForm } = this.state
+            taskImportance, taskDeveloper, ShowAddTaskForm, handleSubmitForm, showDiv } = this.state
+
+
         return (
             <>
+
                 <nav id="nav">
                     <button id="AddTask" onClick={this.handleShowAddTaskForm}><strong>Add  Task</strong></button>
                 </nav>
@@ -85,21 +97,49 @@ class Tasks extends Component {
                     ShowAddTaskForm && <form method="post" onSubmit={handleSubmitForm} className="Form">
                         <button id="close" onClick={this.handleShowCloseTaskForm}>Close</button><br />,
                         <input placeholder="Title" id="input1" onChange={this.ChangeTitle} value={taskTitle} /><br /> <br />,
-                        <input placeholder="Description" onChange={this.ChangeDescription} value={taskDescription} /><br /><br />,
+                        <textarea id="textarea" placeholder="Description" onChange={this.ChangeDescription} value={taskDescription} /><br /><br />,
                         <input type="date" placeholder="StartData" onChange={this.ChangestartDate} value={taskstartDate} /><br /><br />,
                         <input type="date" placeholder="FinishDate" onChange={this.ChangefinishDate} value={taskfinishDate} /><br /><br />,
-                        <input placeholder="Importance" onChange={this.ChangeImportance} value={taskImportance} /><br /><br />,
-                        <input placeholder="Developer" onChange={this.ChangeDeveloper} value={taskDeveloper} /><br /><br />,
+                        <select placeholder="Developer" onChange={this.ChangeDeveloper} value={taskDeveloper}>
+                            <option>Developer</option>
+                            <option>Hovhannes</option>
+                            <option>Dolera</option>
+                            <option>Arman</option>
+                            <option>Eliza</option>
+                        </select><br />,
+                        <div className="Critical">
+                            Critical:
+                            <input type="radio" id="radio" value="hight" name="critical" onChange={this.ChangeImportance} /> Hight
+                            <input type="radio" id="radio" value="middal" name="critical" onChange={this.ChangeImportance} />Middal
+                            <input type="radio" id="radio" value="low" name="critical" onChange={this.ChangeImportance} />Low<br />
+                        </div><br />
                         <div id="button">
                             <input type="submit" value="SUBMIT" id="submit" onClick={this.handleSubmitForm} /><br />
                             <input type="button" value="Reset" id="reset" onClick={this.handleResetForm} /><br />
                         </div>
                     </form>
+
+                }
+                {
+                    showDiv && <div className="tasksContainer">
+                        <h1 className="show">  {taskTitle} </h1>
+                        <h3 className="show1">Developer: {taskDeveloper}</h3>
+                        <h3 className="show1">Importance: {taskImportance}</h3>
+                        <span className="show2"><b>Start:</b> {taskstartDate}</span><br/><br/>
+                        <span className="show2"><b>Finish:</b> {taskfinishDate}</span>
+                        <p className="show3"> <b>Description:</b> {taskDescription}</p>
+                    </div>
+
                 }
             </>
+
         )
+
     }
+
+
 }
+
 
 export default Tasks
 
